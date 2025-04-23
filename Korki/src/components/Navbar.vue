d0f219d5012577220c292785a3cf10c9c68499f8
munio7 <maciej.wrotniak03@gmail.com>
2025-04-23 12:17:56 +0200

    front for first page and navbar

--- Korki/src/components/Navbar.vue
+++ Korki/src/components/Navbar.vue
@@ -1,9 +1,8 @@
 <template>
     <main>
         <div id="logo"></div>
-        <div id="panel">
-            <NButton :text="true" >dada</NButton>
-        </div>
+        <NButton text >Załóż konto</NButton>
+        <NButton text >Zaloguj</NButton>
         <!-- <RouterLink to="/">Home</RouterLink>
         <RouterLink to="/about">About</RouterLink> -->
 
@@ -12,15 +11,20 @@
 
 <script setup lang="ts">
 import { NButton } from 'naive-ui'
+
+
+
 </script>
 
 <style scoped>
-
 main{
     display: flex;
+    align-items: center;
+    justify-content: flex-start;
+    gap: 1rem;
     background-color: var(--navbar-color);
     /* height: calc(100vh - var(--router-width)); */
-    height: 70px;
+    height: var(--navbar-height);
     width: 100%;
 }
 
